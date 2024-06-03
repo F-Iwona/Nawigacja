01c03d886889ae3b71f0ec5bf6549f5d6e82b944
F-Iwona <128170740+F-Iwona@users.noreply.github.com>
2024-06-03 23:37:23 +0200

    Add files via upload

---
+++ DaneGeograficzne.cs
@@ -0,0 +1,13 @@
+using Windows.Devices.Geolocation;
+
+namespace Nawigacja
+{
+    class DaneGeograficzne
+    {
+
+        public static BasicGeoposition pktStartowy;
+        public static BasicGeoposition pktDocelowy;
+        public static string opisCelu = null;
+
+    }
+}
